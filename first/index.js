e1a9ea1b1bf7ec9b8dcc30a09ca98775093fe610
yunsungbin <dbstmdqls0211@gmail.com>
2024-04-22 14:05:11 +0900

    Jump Animation

--- first/index.js
+++ first/index.js
@@ -4,7 +4,7 @@ const c = canvas.getContext("2d"); //2d로 만들겠다는 코드
 canvas.width = 1024;
 canvas.height = 576;
 
-const gravity = 0.5;
+const gravity = 0.7;
 
 const background = new Sprite({
     position : {
@@ -55,6 +55,14 @@ const player = new Fighter({
             imageSrc : "/first/img/1p/Run.png",
             framesMax : 8,
         },
+        jump : {
+            imageSrc : "/first/img/1p/Jump.png",
+            framesMax : 2,
+        },
+        fall : {
+            imageSrc : "/first/img/1p/Fall.png",
+            framesMax : 2,
+        },
     }
 });
 
@@ -87,6 +95,14 @@ const enemy = new Fighter({
         run : {
             imageSrc : "/first/img/1p/Run.png",
             framesMax : 8,
+        },
+        jump : {
+            imageSrc : "/first/img/1p/Jump.png",
+            framesMax : 2,
+        },
+        fall : {
+            imageSrc : "/first/img/1p/Fall.png",
+            framesMax : 2,
         },
     }
 });
@@ -142,24 +158,51 @@ function animate(){
     // else if(keys.d.pressed){
     //     player.velocity.x = +1;
     // }
-    player.image = player.sprites.idle.image;
-
+    //player.image = player.sprites.idle.image;
+    
     if(keys.a.pressed && player.lastKey === "a"){
-        player.image = player.sprites.run.image;
-        player.velocity.x = -2;
+        //player.image = player.sprites.run.image;
+        player.velocity.x = -5;
+        player.switchSprite('run');
     }
     else if(keys.d.pressed && player.lastKey === "d"){
-        player.image = player.sprites.run.image;
-        player.velocity.x = +2;
+        //player.image = player.sprites.run.image;
+        player.velocity.x = +5;
+        player.switchSprite('run');
     }
-    enemy.image = enemy.sprites.idle.image;
+    else{
+        player.switchSprite('idle');
+    }
+
+    if(player.velocity.y < 0){
+        player.switchSprite('jump');
+        // player.image = player.sprites.jump.image;
+        // player.framesMax = player.sprites.jump.framesMax;
+    }
+    else if(player.velocity.y > 0){
+        player.switchSprite('fall');
+    }
+
+    //enemy.image = enemy.sprites.idle.image;
     if(keys.ArrowLeft.pressed && enemy.lastKey === "ArrowLeft"){
-        enemy.image = enemy.sprites.run.image;
-        enemy.velocity.x = -2;
+        enemy.velocity.x = -5;
+        enemy.switchSprite('run');
     }
     else if(keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight"){
-        enemy.image = enemy.sprites.run.image;
-        enemy.velocity.x = +2;
+        enemy.velocity.x = +5;
+        enemy.switchSprite('run');
+    }
+    else{
+        enemy.switchSprite('idle');
+    }
+
+    if(enemy.velocity.y < 0){
+        enemy.switchSprite('jump');
+        // player.image = player.sprites.jump.image;
+        // player.framesMax = player.sprites.jump.framesMax;
+    }
+    else if(enemy.velocity.y > 0){
+        enemy.switchSprite('fall');
     }
 
     if(rectangularColison({rectangle1:player, rectangle2:enemy}) &&
--- first/js/classes.js
+++ first/js/classes.js
@@ -158,6 +158,7 @@ class Fighter extends Sprite{
 
         if(this.position.y + this.height + this.velocity.y >= canvas.height - 96){
             this.velocity.y = 0;
+            this.position.y = 330;
         }
         else{
             this.velocity.y += gravity;
@@ -170,5 +171,42 @@ class Fighter extends Sprite{
             this.isAttacking = false;
         }, 100);
         // 공격 딜레이 걸기 
+    }
+
+    switchSprite (sprite) {
+        switch(sprite){
+            case 'idle':
+                if(this.image !== this.sprites.idle.image){
+                    this.image = this.sprites.idle.image;
+                    this.framesMax = this.sprites.idle.framesMax;
+                    //프레임 초기화.
+                    this.framesCurrent = 0;
+                }
+                break;
+            case 'run':
+                if(this.image !== this.sprites.run.image){
+                    this.image = this.sprites.run.image;
+                    this.framesMax = this.sprites.run.framesMax;
+                    this.framesCurrent = 0;
+                }
+                
+                break;
+            case 'jump':
+                if(this.image !== this.sprites.jump.image){
+                    this.image = this.sprites.jump.image;
+                    this.framesMax = this.sprites.jump.framesMax;
+                    this.framesCurrent = 0;
+                }
+
+                break;
+            case 'fall':
+                if(this.image !== this.sprites.fall.image){
+                    this.image = this.sprites.fall.image;
+                    this.framesMax = this.sprites.fall.framesMax;
+                    this.framesCurrent = 0;
+                }
+
+                break;
+        }
     }
 }
