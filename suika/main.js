43d07ed9f6ed3930c904a523d402ae07ff759624
yunsungbin <dbstmdqls0211@gmail.com>
2024-06-17 15:20:10 +0900

    Remain

--- suika/main.js
+++ suika/main.js
@@ -1,3 +1,5 @@
+import { FRUITS } from "./fruits.js";
+
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
@@ -41,6 +43,7 @@ const ground = Bodies.rectangle(605, 395, 30, 790, {
 
 const topLine = Bodies.rectangle(310, 150, 620, 2, {
     isStatic : true,
+    isSensor : true,
     // 고정시켜주는 옵션
     render : {fillStyle: '#ca8a04'} //#E6B143
 });
@@ -49,3 +52,29 @@ World.add(world, [leftWall, rightWall, ground, topLine]);
 
 Render.run(render);
 Runner.run(engine);
+
+let currentBody = null;
+let currentFruit = null;
+
+
+function addFruit(){
+    const index = Math.floor(Math.random() * 5);
+    console.log(index);
+    const fruit = FRUITS[index];
+
+    const body = Bodies.circle(300, 50, fruit.radius, {
+        index : index,
+        zisSleeping : true,
+        render : {
+            sprite : { texture : `${fruit.name}.png`},
+        },
+        //튀어오르는 강도
+        restitution : 0.2,
+    });
+    World.add(world, body);
+}
+
+currentBody = body;
+currentFruit = fruit;
+
+addFruit();
